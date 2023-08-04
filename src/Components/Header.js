8d79a174703af9916902c2b9d78c96bab991e9ff
Mikhail Elanov <Elanov1991@Gmail.com>
2023-08-04 23:21:12 +0700

    Add router-dom

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,32 +7,40 @@ import {
   FormControl,
   Button,
 } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from "./logo192.png";
 
 export default class Header extends Component {
   render() {
     return (
       <>
-        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+        <Navbar
+          fixed="top"
+          collapseOnSelect
+          expand="md"
+          bg="dark"
+          variant="dark"
+        >
           <Container>
             <Navbar.Brand href="/">
               <img
                 src={logo}
-                height={30}
-                width={30}
+                height="30"
+                width="30"
                 className="d-inline-block align-top"
                 alt="Logo"
-              />
+              />{" "}
+              My WebSite
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
+              <Nav className="me-auto">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/about">About us</Nav.Link>
                 <Nav.Link href="/contacts">Contacts</Nav.Link>
                 <Nav.Link href="/blog">Blog</Nav.Link>
               </Nav>
-              <Form inline="true">
+              <Form className="d-flex">
                 <FormControl
                   type="text"
                   placeholder="Search"
@@ -43,6 +51,15 @@ export default class Header extends Component {
             </Navbar.Collapse>
           </Container>
         </Navbar>
+
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contacts" component={Contacts} />
+            <Route exact path="/blog" component={Blog} />
+          </Switch>
+        </Router>
       </>
     );
   }
